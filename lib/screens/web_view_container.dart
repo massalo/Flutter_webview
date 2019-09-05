48ffdfd1f171f5ab607df690a4b1df7830091343
massalo <massalo@mauanda.com>
2019-09-05 01:59:38 +0100

    Added refreshing on php server side, on client side failed with a bang, refreshing is still an issue and I'll php cheat. although the clearcache() and reload() shouldve worked....

--- lib/screens/web_view_container.dart
+++ lib/screens/web_view_container.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -10,48 +12,49 @@ class WebViewContainer extends StatefulWidget {
   createState() => _WebViewContainerState(this.url1);
 }
 
-//here
-class WebViewContainer2 extends StatefulWidget {
-  final url1;
-  //final url2;
-  WebViewContainer2(this.url1);
-
-  @override
-  createState() => _WebViewContainerState(this.url1);
-}
-
 class _WebViewContainerState extends State<WebViewContainer> {
   var _url;
   final _key = UniqueKey();
-
   _WebViewContainerState(this._url);
 
-  var controller = _WebViewContainerState;
-
-//TRYING TO FIGURE OUT HOW TO MAKE IT REFRESH THE WEBVIEW, right now it does nothing
-  //https://stackoverflow.com/questions/57215843/how-to-reload-webview-in-flutter
-  void _getRefreshWebsite() {
-    setState(() {
-      //_url = ;
-      // this.load;
-    });
-  }
-
-  // WebViewContainer;
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(children: [
-        Expanded(
-            child: WebView(
-                key: _key,
-                javascriptMode: JavascriptMode.unrestricted,
-                initialUrl: _url))
-      ]),
-      floatingActionButton: FloatingActionButton(
-          onPressed: _getRefreshWebsite, child: Icon(Icons.refresh)),
-    );
+        appBar: AppBar(),
+        body: Column(children: [
+          Expanded(
+              child: WebView(
+            key: _key,
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: _url,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+          ))
+        ]),
+        floatingActionButton: FutureBuilder<WebViewController>(
+            future: _controller.future,
+            builder: (BuildContext context,
+                AsyncSnapshot<WebViewController> snapshot) {
+              final bool webViewReady =
+                  snapshot.connectionState == ConnectionState.done;
+              final WebViewController controller = snapshot.data;
+
+              if (snapshot.hasData) {
+                return FloatingActionButton(
+                    child: Icon(Icons.arrow_forward),
+                    onPressed: !webViewReady
+                        ? null
+                        : () async {
+                            controller.clearCache();
+                            controller.reload();
+                          });
+              }
+
+              return Container();
+            }));
   }
 }
