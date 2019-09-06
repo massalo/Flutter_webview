17b7a5e704c0608308b3b934f41d95e29c71d533
massalo <massalo@mauanda.com>
2019-09-06 03:36:47 +0100

    Finally resolved the PHP issue, added about page and other quirks

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'web_view_container.dart';
+import 'howtoplay.dart';
 
 class Home extends StatelessWidget {
-  final links1 = 'http://puroangola.com/ojogo/getData.php';
-  final links2 = 'http://puroangola.com/';
+  final links2 = 'https://puroangola.com/cafe/';
 
-  String get url1 => links1.toString();
   String get url2 => links2.toString();
 
   @override
@@ -21,7 +20,7 @@ class Home extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              (_urlButton1(context, url1)),
+              (urlButton1(context)),
               (_urlButton2(context, url2))
             ],
           ),
@@ -30,27 +29,26 @@ class Home extends StatelessWidget {
     );
   }
 
-  //buttons
-  Widget _urlButton1(BuildContext context, String url1) {
+  //BUTTON 1 COMO JOGAR --> HOWTOPLAY
+  Widget urlButton1(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20.0),
       child: FlatButton(
         color: Theme.of(context).primaryColor,
         padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
         //here the buttons use either the url or a "title", ths problem is ging differente "titles"
-        child: Text('Casais'),
+        child: Text('Como Jogar'),
         textColor: Colors.white,
-        onPressed: () => _handleURLButtonPress1(context, url1),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Howtoplay()));
+        },
+//            _handleURLButtonPress1(context, url1),
       ),
     );
   }
 
-  //calls the url1
-  void _handleURLButtonPress1(BuildContext context, String url1) {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => WebViewContainer(url1)));
-  }
-
+//BUTTON 2 --> O JOGO COMECAR
   Widget _urlButton2(BuildContext context, String url2) {
     return Container(
       padding: EdgeInsets.all(20.0),
@@ -58,7 +56,7 @@ class Home extends StatelessWidget {
         color: Theme.of(context).primaryColor,
         padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
         //here the buttons use either the url or a "title", ths problem is ging differente "titles"
-        child: Text('Amigos'),
+        child: Text('Começar'),
         textColor: Colors.white,
         onPressed: () => _handleURLButtonPress2(context, url2),
       ),
